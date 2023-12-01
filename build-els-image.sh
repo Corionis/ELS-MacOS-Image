f6f8fc62007c27bb810b2fd8a6fafa6b457f659e
Corionis <rockney8588@gmail.com>
2023-12-01 11:25:36 -0700

    #139 Working version of the macOS DMG imager; More test required

--- build-els-image.sh
+++ build-els-image.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 echo ""
-echo "Build ELS DMG Image"
+echo "Build ELS & Updater DMG Images"
 echo ""
 
 if [ ! -d ELS/ ]; then
     echo "ELS/ directory is missing"
+    exit 1
+fi
+
+if [ ! -d ELS_Updater/ ]; then
+    echo "ELS_Updater/ directory is missing"
     exit 1
 fi
 
@@ -14,12 +19,15 @@ if [ ! -f ELS/build-stamp.properties ]; then
     exit 1
 fi
 
-# Build ELS.app using com.oracle.appbundler.AppBundlerTask
+# Build ELS.app & ELS_Updater.app using com.oracle.appbundler.AppBundlerTask
 # https://github.com/TheInfiniteKind/appbundler
 # That library must be added to the Ant lib/
-ant -f els-app.xml
+ant -f els-1-app.xml
 
-# Build the DMG from the .app using DMG Canvas
+# Build the DMGs from the .apps using DMG Canvas
 # https://www.araelium.com/dmgcanvas
-dmgcanvas "Entertainment Library Synchronizer.dmgcanvas/" ELS.dmg
+dmgcanvas "dmgcanvas/ELS.dmgcanvas/" build/ELS-macos.dmg
+dmgcanvas "dmgcanvas/ELS_Updater.dmgcanvas/" build/ELS-macos-updater.dmg
 
+# Rename using build-stamp.properties
+ant -f els-2-rename.xml
